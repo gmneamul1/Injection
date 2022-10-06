326aa1478cba6df6c1182ac3dec3c9dfd6ece471
gmneamul1 <gmneamul1@gmail.com>
2022-10-07 00:41:54 +0600

    funtion add

--- index.js
+++ index.js
@@ -1,79 +1,117 @@
 document.getElementById("calulator").addEventListener("click", function () {
-
-
-    // cyce time 
+  // cyce time
   const cycleTime = document.getElementById("cycle").value;
 
-  const cycleString = parseFloat(cycleTime ? cycleTime:1);
-  const output = 345600 / cycleString;
-  const outputString = parseInt(output);
+  const cycleString = parseFloat(cycleTime ? cycleTime : 0);
+  // console.log(cycleString)
+  // let output = 0;
+   function outputvalue() {
+    if (cycleString <= 0) {
+      return 0;
+    } else {
+      let output = 345600 / cycleString;
+      // console.log(output);
+      return output;
+    }
+  }
+  const perHourOutput = outputvalue();
+  // console.log(perHourOutput);
+
+  const outputString = parseInt(perHourOutput);
   const outputText = document.getElementById("output");
-  outputText.innerText = outputString;
+  outputText.innerText = perHourOutput;
   const outputValue = outputText.innerText;
-  
-  const material = document.getElementById('size').value;
-  const gramToKg = parseFloat(material /1000);
 
-// console.log(gramToKg)
-  const materials = (material )* outputString 
-  const gramMaterial = (material )* outputString 
-  const  kgMaterial = gramMaterial /1000;
-  const materialskg= document.getElementById('material-kg');
+  const material = document.getElementById("size").value;
+  const gramToKg = parseFloat(material / 1000);
 
-  materialskg.innerText =parseFloat(kgMaterial).toFixed(2);
-  const remainingTime = document.getElementById('remaining-time').value;
+  // console.log(gramToKg)
+  const materials = material * outputString;
+  const gramMaterial = material * outputString;
 
-  const reminingTimeNumber =60 / remainingTime;
-  const remaingstring = parseInt(reminingTimeNumber);
-  
-  const remaingPreforms = outputString / remaingstring;
-  const product8hourse =document.getElementById('8hoursProduct');
-  const producedPreforms = document.getElementById('produce-preforms').value
-//   total production 
-  product8hourse.innerText = parseInt(remaingPreforms)  + parseInt(producedPreforms? producedPreforms:0)
-  const totalProduction =   product8hourse.innerText
-console.log(totalProduction)
+  console.log(material)
+  console.log(outputString)
+  console.log(gramMaterial)
+  const kgMaterial = gramMaterial / 1000;
+  const materialskg = document.getElementById("material-kg");
 
-    const totalWastage =document.getElementById('wastage').value;
-    const userWastege = parseFloat(totalWastage?  totalWastage: 0)
-    // console.log(userWastege)
-
-  const produceToMaterials = totalProduction  * gramToKg  ;
-  const materialsString =parseFloat(produceToMaterials)
-//   console.log(produceToMaterials)
-const TotalMaterial = userWastege + materialsString;
-console.log(TotalMaterial)
-  
-
-  const totalMaterial = document.getElementById('total-materials')
-  totalMaterial.innerText= TotalMaterial.toFixed(2)
-
-  const perCase = document.getElementById('perCase').value
-
-  const cases = document.getElementById("case");
-  const totalCase = parseInt(totalProduction/ perCase? perCase:1);
-  cases.innerText = totalCase;
-//   carton
-const carton = document.getElementById('percarton').value
-const cartons = parseFloat((carton? carton: 0))
-const totalCarton =  parseFloat(totalProduction)  / parseInt(cartons);
-const totalCartons = document.getElementById('Cartons')
-totalCartons.innerText = totalCarton
-
-// cages
-const cages = document.getElementById('per-cage').value;
-
-const totalCages = document.getElementById('cages');
-const totalCage =(cages? cages:1)
-
-totalCages.innerText = parseInt( totalCarton)  / parseInt(totalCage);
+  materialskg.innerText = parseFloat(kgMaterial).toFixed(2);
+  const remainingTime = document.getElementById("remaining-time").value;
+  const remain = remainingTime ? remainingTime : 0;
  
 
+function remaingTime (){
+  if(remain <= 0){
+    return 0;
+  }else{
+    const reminingTimeNumber = 60 / remainingTime;
+    // console.log(reminingTimeNumber)
+    return reminingTimeNumber;
+  }
+}
+const remainTime =  remaingTime();
+// console.log(remainTime)
 
+  
+  const remaingstring = parseInt(remainTime );
+  // console.log(remaingstring)
+const outputZero =(perHourOutput ? perHourOutput:0)
 
+function remainoutput () {
+if(remaingstring <=0 ){
+  return 0
+  
+}else{
+  const remaingPreforms =outputZero / remaingstring;
+  return remaingPreforms
+}
 
+}
+const remainPreform =remainoutput()
+// console.log(remainPreform)
+
+  const product8hourse = document.getElementById("8hoursProduct");
+  const producedPreforms = document.getElementById("produce-preforms").value;
+  //   total production
+  product8hourse.innerText =parseInt(remainPreform) +
+parseInt(producedPreforms ? producedPreforms : 0);
+  const totalProduction = product8hourse.innerText;
+  // console.log(totalProduction)
+
+  const totalWastage = document.getElementById("wastage").value;
+  const userWastege = parseFloat(totalWastage ? totalWastage : 0);
+  // console.log(userWastege)
+
+  const produceToMaterials = totalProduction * gramToKg;
+  const materialsString = parseFloat(produceToMaterials);
+  //   console.log(produceToMaterials)
+  const TotalMaterial = userWastege + materialsString;
+  // console.log(TotalMaterial)
+
+  const totalMaterial = document.getElementById("total-materials");
+  totalMaterial.innerText = TotalMaterial.toFixed(2);
+
+  const perCase = document.getElementById("perCase").value;
+
+  const cases = document.getElementById("case");
+  const totalCase = parseInt(totalProduction / perCase ? perCase : 1);
+  cases.innerText = totalCase;
+  //   carton
+  const carton = document.getElementById("percarton").value;
+  const cartons = parseFloat(carton ? carton : 0);
+  const totalCarton = parseFloat(totalProduction) / parseInt(cartons);
+  const totalCartons = document.getElementById("Cartons");
+  totalCartons.innerText = totalCarton;
+
+  // cages
+  const cages = document.getElementById("per-cage").value;
+
+  const totalCages = document.getElementById("cages");
+  const totalCage = cages ? cages : 1;
+
+  totalCages.innerText = parseInt(totalCarton) / parseInt(totalCage);
 });
 
-function refreshPage(){
-    window.location.reload();
-} 
+function refreshPage() {
+  window.location.reload();
+}
